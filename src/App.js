939e2f004362340f536e354f566f17c3f7b186f1
Zanele Kunene <zanele.kunene@cdasolutions.co.za>
2025-08-22 15:19:27 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Box,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
+import LoginPage from "./LoginPage";
+import SignUpPage from "./SignUpPage";
+import { Route, Router, Routes } from "react-router-dom";
+import IceCreamShop from "./IceCreamShop";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage/>}/>
+      <Route path="/Home" element={<IceCreamShop/>}/>
+    </Routes>
   );
 }
 
